326f61f26626125b70c8ce7c36e8bdaa844f08cc
ekambalov <emakcomp@gmail.com>
2023-03-19 10:03:20 +0100

    feat: add comments feat: change .gitignore

--- calculete/index.js
+++ calculete/index.js
@@ -1,10 +1,19 @@
+//conecting libraryes
+
 const fs = require("fs");
 const path = require("path");
 const { stdin, stdout, exit } = process;
 const { readdir, stat } = require("fs/promises");
 
+// finding our ass directory
 const assPath = path.join(__dirname, "../ass");
 
+//function that get string and file directory and write string to file
+const writeTofile = (text, dir) => {
+  const writeStream = fs.createWriteStream(path.join(__dirname, `${dir}`), "utf-8");
+  writeStream.write(text);
+};
+//getting all .ass filenames
 const getFiles = async (dirPath) => {
   const files = await readdir(dirPath, { withFileTypes: true });
   const assNames = [];
@@ -12,12 +21,13 @@ const getFiles = async (dirPath) => {
     if (file.isFile()) {
       const stats = await stat(path.join(dirPath, file.name));
       const fileParse = path.parse(path.join(dirPath, file.name));
-      assNames.push(`${fileParse.name}`);
+      if (fileParse.ext === ".ass") assNames.push(`${fileParse.name}`); //check if file is .ass
     }
   }
   return assNames;
 };
 
+//function that delete all empty elements in array
 const rowCleaner = (arr) => {
   let newArr = [...arr];
   let emptyRows = 0;
@@ -30,11 +40,12 @@ const rowCleaner = (arr) => {
   return newArr;
 };
 
+//function that get sub and return only rows with setting roles
 const getAssRoles = (sub, ...role) => {
   if (!role[0]) return sub;
-  const subStr = sub.toString();
+  const subString = sub.toString();
   const newSubs = rowCleaner(
-    subStr.split("\n").map((row) => {
+    subString.split("\n").map((row) => {
       if (row.slice(0, 8) === "Dialogue") {
         const newRow = row.split(",");
         if (role.includes(newRow[4])) {
@@ -53,30 +64,38 @@ const getAssRoles = (sub, ...role) => {
   return newSubs;
 };
 
+//convert ass to srt
 const srtConverter = (assSubs) => {
+  //convert time from ass format to srt format
   const timeConverter = (oldTime) => {
     return oldTime.slice(0, 7) + "," + oldTime.slice(8, 10) + "0";
   };
 
+  //split ass file by rows, and return only rows with dialogue
   const assRows = assSubs.split("\n").filter((row) => row.slice(0, 8) === "Dialogue");
-  let strRow = [`1\n00:00:00,000 --> 00:00:00,600\ntest\n`];
+
+  let strRow = [];
   for (let i = 0; i < assRows.length; i++) {
     const rowParam = assRows[i].split(",");
     let startTime = timeConverter(rowParam[1]);
     let endTime = timeConverter(rowParam[2]);
 
     let text = rowParam.slice(9).join(",");
+    //changing \N to \n to separrate rows
     if (text.indexOf("\\N") !== -1) {
       text = text.slice(0, text.indexOf("\\N")) + "\n" + text.slice(text.indexOf("\\N") + 2);
     }
-    strRow.push(`${i + 2}\n${startTime} --> ${endTime}\n${text}\n`);
+    strRow.push(`${i + 1}\n${startTime} --> ${endTime}\n${text}\n`);
   }
   return strRow.join("\n");
 };
 
-const getRoles = async () => {
-  const stream = fs.createReadStream(path.join(__dirname, "../", `roles.txt`), "utf-8");
+//main code program
 
+(async () => {
+  //making stream to get roles:
+  const stream = fs.createReadStream(path.join(__dirname, "../", `roles.txt`), "utf-8");
+  //get array with roles:
   let data = "";
   stream.on("data", (chunk) => (data += chunk));
   stream.on("end", () => {
@@ -86,13 +105,11 @@ const getRoles = async () => {
       .map((item) => item.trim());
 
     (async () => {
-      const subNames = await getFiles(assPath);
+      const subNames = await getFiles(assPath); //getting all .ass filenames in array
+
+      //starting script for all .ass files
       subNames.forEach((subName) => {
         const stream = fs.createReadStream(path.join(__dirname, "../ass", `${subName}.ass`), "utf-8");
-        const writeTofile = (text, dir) => {
-          const writeStream = fs.createWriteStream(path.join(__dirname, `${dir}`), "utf-8");
-          writeStream.write(text);
-        };
         let data = "";
         stream.on("data", (chunk) => (data += chunk));
         stream.on("end", () => {
@@ -102,5 +119,4 @@ const getRoles = async () => {
       });
     })();
   });
-};
-getRoles();
+})();
