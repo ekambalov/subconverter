14c066f4042fe905acbf248bf2bd8b81f6ee12cb
ekambalov <emakcomp@gmail.com>
2023-03-19 09:02:26 +0100

    feat: delete test.js

--- calculete/test.js
+++
@@ -1,5 +0,0 @@
-let text = "Калі гэта праца Кіры, значыць, ён не ведае нашых\\Nімёнаў і твараў, каб забіць, таму забіў Такімуру.";
-if (text.indexOf("\\N") !== -1) {
-  text = text.slice(0, text.indexOf("\\N")) + "\n" + text.slice(text.indexOf("\\N") + 2);
-}
-console.log(text);
